1e912d67650e06e8db4e0292358ce5d6681d9022
lucidph3nx <ben@tinfoilforest.nz>
2023-08-05 11:20:56 +1200

    I want to see which key less often

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,4 +1,9 @@
 return {
   'folke/which-key.nvim',
+  init = function()
+    vim.o.timeout = true
+    -- longer timeout, because i feel it shows to often
+    vim.o.timeoutlen = 1000
+  end,
   opts = {}
 }
