2a73a7306096ca27126aea34a49c6e3bb839ea44
lucidph3nx <ben@tinfoilforest.nz>
2023-08-08 22:17:35 +1200

    chatgpt plugin

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,126 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
+  },
+  keys = {
+    { '<C-c>', vim.cmd.ChatGPT,  desc = 'Open ChatGPT' },
+  },
+  opts = {
+    chat = {
+      welcome_message = WELCOME_MESSAGE,
+      loading_text = "Loading, please wait ...",
+      question_sign = "",
+      answer_sign = "ﮧ",
+      max_line_length = 120,
+      sessions_window = {
+        border = {
+          highlight = "Normal",
+          style = "single",
+          text = {
+            top = " Sessions ",
+          },
+        },
+        win_options = {
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+        },
+      },
+      keymaps = {
+        close = { "<C-c>" },
+        yank_last = "<C-y>",
+        yank_last_code = "<C-k>",
+        scroll_up = "<C-u>",
+        scroll_down = "<C-d>",
+        new_session = "<C-n>",
+        cycle_windows = "<Tab>",
+        cycle_modes = "<C-f>",
+        select_session = "<Space>",
+        rename_session = "r",
+        delete_session = "d",
+        draft_message = "<C-d>",
+        toggle_settings = "<C-o>",
+        toggle_message_role = "<C-r>",
+        toggle_system_role_open = "<C-s>",
+      },
+    },
+    popup_window = {
+      border = {
+        highlight = "Normal",
+        style = "single",
+        text = {
+          top = " ChatGPT ",
+        },
+      },
+      win_options = {
+        wrap = true,
+        linebreak = true,
+        foldcolumn = "1",
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+      buf_options = {
+        filetype = "markdown",
+      },
+    },
+    popup_input = {
+      prompt = "  ",
+      border = {
+        highlight = "Normal",
+        style = "single",
+        text = {
+          top_align = "center",
+          top = " Prompt ",
+        },
+      },
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+      submit = "<Enter>",
+      submit_n = "<Enter>",
+    },
+    settings_window = {
+      border = {
+        highlight = "Normal",
+        style = "single",
+        text = {
+          top = " Settings ",
+        },
+      },
+      win_options = {
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+    },
+    system_window = {
+      border = {
+        highlight = "Normal",
+        style = "single",
+        text = {
+          top = " SYSTEM ",
+        },
+      },
+      win_options = {
+        wrap = true,
+        linebreak = true,
+        foldcolumn = "2",
+        winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+      },
+    },
+    openai_params = {
+      model = "gpt-3.5-turbo",
+      frequency_penalty = 0,
+      presence_penalty = 0,
+      max_tokens = 300,
+      temperature = 0,
+      top_p = 1,
+      n = 1,
+    },
+    openai_edit_params = {
+      model = "code-davinci-edit-001",
+      temperature = 0,
+      top_p = 1,
+      n = 1,
+    },
+  },
+}
