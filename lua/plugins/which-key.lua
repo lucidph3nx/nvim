db6cd8815f7699caef63d85867c57a91324a34f8
lucidph3nx <ben@tinfoilforest.nz>
2023-08-24 07:48:09 +1200

    remove whichkey

--- lua/plugins/which-key.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'folke/which-key.nvim',
-  init = function()
-    vim.o.timeout = true
-    -- longer timeout, because i feel it shows to often
-    vim.o.timeoutlen = 1000
-  end,
-  opts = {}
-}
