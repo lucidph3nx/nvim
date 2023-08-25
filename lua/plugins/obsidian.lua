f9e74fb0e6ddd8781c75cef3f8fcdaef6c9a795d
lucidph3nx <ben@tinfoilforest.nz>
2023-08-26 09:17:19 +1200

    allow obsidian follow to open urls

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -6,12 +6,12 @@ return {
     'nvim-lua/plenary.nvim',
   },
   keys = {
-    { '<leader>od', vim.cmd.ObsidianToday,     desc = '[O]bsidian [D]aily note for Today' },
-    { '<leader>oy', vim.cmd.ObsidianYesterday, desc = '[O]bsidian [Y]esterday' },
-    { '<leader>oo', vim.cmd.ObsidianOpen,      desc = '[O]bsidian [O]pen' },
-    { '<leader>ot', vim.cmd.ObsidianTemplate,  desc = '[O]bsidian [T]emplate' },
-    { '<leader>on', vim.cmd.ObsidianNew,  desc = '[O]bsidian [N]ew' },
-    { '<leader>of', vim.cmd.ObsidianFollowLink,  desc = '[O]bsidian [F]ollow Link' },
+    { '<leader>od', vim.cmd.ObsidianToday,      desc = '[O]bsidian [D]aily note for Today' },
+    { '<leader>oy', vim.cmd.ObsidianYesterday,  desc = '[O]bsidian [Y]esterday' },
+    { '<leader>oo', vim.cmd.ObsidianOpen,       desc = '[O]bsidian [O]pen' },
+    { '<leader>ot', vim.cmd.ObsidianTemplate,   desc = '[O]bsidian [T]emplate' },
+    { '<leader>on', vim.cmd.ObsidianNew,        desc = '[O]bsidian [N]ew' },
+    { '<leader>of', vim.cmd.ObsidianFollowLink, desc = '[O]bsidian [F]ollow Link' },
   },
   opts = {
     dir = '~/documents/obsidian/personal-vault',
@@ -23,6 +23,9 @@ return {
     templates = {
       subdir = 'templates',
     },
+    follow_url_func = function(url)
+      vim.fn.jobstart({"xdg-open", url})
+    end,
     completion = {
       nvim_cmp = true,
       new_notes_location = 'notes_subdir',
