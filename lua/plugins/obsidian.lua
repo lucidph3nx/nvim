3d40307fd094a98dc143cebc302e28cfafe088c7
lucidph3nx <ben@tinfoilforest.nz>
2023-10-27 16:05:51 +1300

    changes to obsidian.nvim config

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -12,6 +12,7 @@ return {
     { '<leader>ot', vim.cmd.ObsidianTemplate,   desc = '[O]bsidian [T]emplate' },
     { '<leader>on', vim.cmd.ObsidianNew,        desc = '[O]bsidian [N]ew' },
     { '<leader>of', vim.cmd.ObsidianFollowLink, desc = '[O]bsidian [F]ollow Link' },
+    { '<leader>ob', vim.cmd.ObsidianBacklink,   desc = '[O]bsidian show [B]acklinks' },
   },
   opts = {
     dir = '~/documents/obsidian/personal-vault',
@@ -29,6 +30,7 @@ return {
     completion = {
       nvim_cmp = true,
       new_notes_location = 'notes_subdir',
+      prepend_note_id = false,
     },
     open_app_foreground = true,
     disable_frontmatter = true,
@@ -51,6 +53,23 @@ return {
           -- Run ObsidianTemplate command
           vim.cmd('ObsidianTemplate daily_note.md')
         end
+      end,
+    })
+    -- autocommand to set keybindings only in obsidian backlinks buffer
+    vim.api.nvim_create_autocmd('FileType', {
+      group = augroup,
+      pattern = '*',
+      callback = function()
+        -- only applies to files with filetype ObsidianBacklinks
+        if vim.bo.ft ~= "ObsidianBacklinks" then
+          return
+        end
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        -- quit buffer
+        vim.keymap.set('n', 'q', function()
+          vim.cmd('q')
+        end, { buffer = bufnr, desc = '[Q]uit' })
       end,
     })
   end
