d85d7d721596a3f57490271fe50c9f1abb2368e2
lucidph3nx <ben@tinfoilforest.nz>
2023-11-09 12:46:00 +1300

    increase gpt max tokens

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -111,7 +111,7 @@ return {
       model = "gpt-3.5-turbo",
       frequency_penalty = 0,
       presence_penalty = 0,
-      max_tokens = 300,
+      max_tokens = 600,
       temperature = 0,
       top_p = 1,
       n = 1,
