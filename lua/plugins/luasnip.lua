5c36c5e3ec140d6eae166545b41e1cb3934d18d9
lucidph3nx <ben@tinfoilforest.nz>
2024-02-17 20:06:19 +1300

    add snippet to create a wiki date link

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,10 +4,18 @@ return {
   config = function()
     local ls = require("luasnip")
     local s = ls.snippet
+    local sn = ls.snippet_node
     local t = ls.text_node
+    local d = ls.dynamic_node
     -- my snippets
     ls.add_snippets("markdown", {
       s("[] ", { t("- [ ] ") })
+    })
+    ls.add_snippets("markdown", {
+      s("wikidate", d(1, function (args, parent)
+      local env = parent.snippet.env
+      return sn(nil, t{"[[" .. env.CURRENT_YEAR .. "-" .. env.CURRENT_MONTH .. "-" .. env.CURRENT_DATE .. "]]"})
+    end, {}))
     })
   end,
 }
