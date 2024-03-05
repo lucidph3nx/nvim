4ba9243e2867a7dd74842cbed7e3d93b29e3fef4
lucidph3nx <ben@tinfoilforest.nz>
2024-03-05 20:47:01 +1300

    better formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/conform.nvim',
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+}
