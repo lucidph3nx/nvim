ca26728c7a1ef2078b76a51d8aacd5201447b25a
lucidph3nx <ben@tinfoilforest.nz>
2024-04-11 16:59:53 +1200

    config for conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/conform.nvim',
-  event = { "BufWritePre" },
+  -- event = { "BufWritePre" },
   cmd = { "ConformInfo" },
   keys = {
     {
@@ -12,5 +12,12 @@ return {
       mode = "",
       desc = "Format buffer",
     },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+      nix = { "alejandra" },
+    }
   },
 }
