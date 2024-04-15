d6b4b5c2d3670c1dc3ff333c563f1d24ee87d1df
lucidph3nx <ben@tinfoilforest.nz>
2024-04-15 21:15:52 +1200

    add oil.nvim

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  'stevearc/oil.nvim',
+  keys = {
+    { '-', "<Cmd>Oil<CR>", desc = "open parent directory" },
+  },
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
